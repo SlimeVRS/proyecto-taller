9b61524f848bd68f54fdac26c21b53d046d6ede8
Dennis Jimenez <alejimenezc@hotmail.com>
2023-11-19 22:04:29 -0600

    ALU controller and decoder

---
+++ ALU.sv
@@ -0,0 +1,9 @@
+module ALU(input logic[31:0] SrcA, 
+			  input logic[31:0] SrcB, 
+			  input logic [2:0]ALUControl,
+			  output logic[3:0]ALUFlags, 
+			  output logic[31:0] ALUResult );
+
+	ALUComponent ALUComponent(SrcA,SrcB,ALUControl, ALUFlags, ALUResult);
+	
+endmodule
