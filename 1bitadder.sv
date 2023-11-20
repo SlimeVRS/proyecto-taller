8990d8a7a66579ec740400215d106b7c03058f32
Dennis Jimenez <alejimenezc@hotmail.com>
2023-11-19 19:33:49 -0600

    Componentes ALU

---
+++ 1bitadder.sv
@@ -0,0 +1,7 @@
+module 1bitadder(input logic carry_in, A,B,
+					  output logic S, carry_on);
+	wire AB,AC,BC;
+	xor salida(S,A,B,carry_in);
+	and and_AB(AB,A,B),and_AC(AC,A,carry_in),and_BC(BC,B,carry_in);
+	or or_carry(carry_on,AB,AC,BC);
+endmodule
